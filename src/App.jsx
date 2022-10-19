9e6eec5618474b0104aef1d21b6f42ae34d5a5c3
Pedro Castro <pgkastro@gmail.com>
2022-10-19 13:56:12 -0300

    Criando Projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import main from './assets/components/main';
+import form from './assets/components/form';
+import button from './assets/components/button';
+
+function App() {
+<div>
+    
+</div>
+}
+
+export default App
