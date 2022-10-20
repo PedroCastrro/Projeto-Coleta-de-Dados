cc5f0514aa662025c65e7fa19eeace3822baf483
Pedro Castro <pgkastro@gmail.com>
2022-10-20 18:55:59 -0300

    first card

--- src/assets/components/Home/index.jsx
+++ src/assets/components/Home/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './styles.css';
-import CardFront from './imagens/bg-card-front.png';
 
 
 
 export function Home() {
     return (
-        <>
-        <img src={ CardFront } alt="" />
-        </>
+        <div className='img'>
+        <img src="bg-card-front.png"  id="img1" alt="" />
+        <label id="numero">9591 6489 6389 101E</label>
+        <label id="nome">Felicia Leire</label>
+        <label id="card">09/00</label>
+        </div> 
     )
 }
 
